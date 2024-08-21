748232b3cd2f5df47f125424b293e1169427cf48
wendywen20 <wenyimei6699@gmail.com>
2024-08-20 21:22:13 -0400

    error

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const CourseInfo = {
         id: 1,
         name: "Declare a Variable",
         due_at: "2023-01-25",
-        points_possible: 50
-      },
+        points_possible: 0
+           },
       {
         id: 2,
         name: "Write a Function",
@@ -96,69 +96,96 @@ const CourseInfo = {
     return result;
   }
   
+
+
+
+
+  
+
+
+ 
   const result = getLearnerData(CourseInfo, AssignmentGroup, [LearnerSubmissions]);
 
   function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions){
   // If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. 
-  console.log(AssignmentGroup['course_id'] !== CourseInfo['id'])
-  try { AssignmentGroup['course_id'] !== CourseInfo['id'] }
-  catch (error) {console.log(`The assignment is not for Course ${CourseInfo['id']}`)}
-
-//Assignment by id 
-  const assignmentsById = {};
-  for (let i = 0; i < AssignmentGroup.assignments.length; i++) {
-    const assignment = AssignmentGroup.assignments[i];
-    assignmentsById[assignment.id] = assignment;
+  console.log(AssignmentGroup.course_id !== CourseInfo.id)
+  function validClass(AssignmentCourse_id, CourseInfoID){
+   if (AssignmentCourse_id !== CourseInfoID ){
+    console.error(`The assignment is not for Course ${CourseInfo['id']}`)}
   }
-//Learner Assignment id
-    const learnerData = {};
-    for (let i = 0; i < LearnerSubmissions.length; i++) {
-      const submission = LearnerSubmissions[i];
-      const { learner_id, assignment_id, submission: submissionData } = submission;
+  validClass(AssignmentGroup.course_id, CourseInfo.id)
 
-      if (!(LearnerSubmissions.assignment_id in assignmentsById)) {
-        throw new Error(`Assignment ${LearnerSubmissions.assignment_id} does not exist in the provided AssignmentGroup.`);
-      }
+  function validPoints(AssignmentGroupPoints){
+
+    AssignmentGroup.assignments.forEach((AssignmentGroupPoints) => {console.log(AssignmentGroupPoints);
+      const points=AssignmentGroupPoints.points_possible;
+      console.log(points)
+      if (points === 0){
+        console.error(`The Assignment points for ${AssignmentGroup.assignments[2]} should be more than zero`)} // there is an error here the console print out The Assignment points for [object Object] should be more than zeroc
+        
+    })}
+
+  validPoints(AssignmentGroup.assignments.points_possible)
 
 
 
-      const dueDate = new Date(AssignmentGroup.assignments.due_at);
-      const submittedDate = new Date(LearnerSubmissions.submission.submitted_at);
 
-      // Skip assignments not yet due
-      if (dueDate > new Date()) {
-        continue;
-      }
+}
+  //You should also account for potential errors in the data that your program receives. What if
+// points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number isinstead a string?
+ 
 
-      // Calculate the score considering lateness
-      const score = calculateScore(submissionData.score, assignment.points_possible, submittedDate, dueDate);
+// //Assignment by id 
+//   const assignmentsById = {};
+//   for (let i = 0; i < AssignmentGroup.assignments.length; i++) {
+//     const assignment = AssignmentGroup.assignments[i];
+//     assignmentsById[assignment.id] = assignment;
+//   }
+// //Learner Assignment id
+//     const learnerData = {};
+//     for (let i = 0; i < LearnerSubmissions.length; i++) {
+//       const submission = LearnerSubmissions[i];
+//       const { learner_id, assignment_id, submission: submissionData } = submission;
 
-      // Initialize learner data if not present
-      if (!learnerData[learner_id]) {
-        learnerData[learner_id] = {
-          id: learner_id,
-          avg: 0,
-          total_weighted_score: 0,
-          total_points_possible: 0,
-          assignments: {}
-        };
-      }
+//       if (!(LearnerSubmissions.assignment_id in assignmentsById)) {
+//         throw new Error(`Assignment ${LearnerSubmissions.assignment_id} does not exist in the provided AssignmentGroup.`);
+//       }
 
-      const percentageScore = (score / assignment.points_possible) * 100;
 
-      // Save the percentage score for this assignment
-      learnerData[learner_id].assignments[assignment_id] = percentageScore;
-    }
 
-  // Create a mapping from learner ID to their submissions
+//       const dueDate = new Date(AssignmentGroup.assignments.due_at);
+//       const submittedDate = new Date(LearnerSubmissions.submission.submitted_at);
+
+//       // Skip assignments not yet due
+//       if (dueDate > new Date()) {
+//         continue;
+//       }
+
+//       // Calculate the score considering lateness
+//       const score = calculateScore(submissionData.score, AssignmentGroup.assignments.points_possible, submittedDate, dueDate);
+
+//       // Initialize learner data if not present
+//       if (!learnerData[learner_id]) {
+//         learnerData[learner_id] = {
+//           id: learner_id,
+//           avg: 0,
+//           total_weighted_score: 0,
+//           total_points_possible: 0,
+//           assignments: {}
+//         };
+//       }
+
+//       const percentageScore = (submissionData.score /AssignmentGroup.assignments.points_possible) * 100;
+
+//       // Save the percentage score for this assignment
+//       learnerData[learner_id].assignments[assignment_id] = percentageScore;
+//     }
+
+//   // Create a mapping from learner ID to their submissions
 
   // for (let i = 0; i < LearnerSubmissions.length; i++) {
   //   const submission = LearnerSubmissions[i];
   //   const { learner_id, assignment_id, submission: submissionData } = submission;
-  // }
+ 
 
-
-
-  }
-  getLearnerData()
   // console.log(result);
